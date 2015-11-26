a06e295f613dee6244d792499dcc86a24575adec
kelvinmbwilo <kelvinmbwilo@gmial.com>
2015-11-26 15:51:00 +0300

    introduce family plannig page

--- controllers/FamilyPlanningCtrl.js
+++ controllers/FamilyPlanningCtrl.js
@@ -4,8 +4,9 @@
 
 angular.module("hmisPortal")
     .config(function($httpProvider) {
-        $httpProvider.defaults.withCredentials = true;
+
     })
     .controller("FamilyPlanningCtrl",function ($rootScope,$scope,$http,$location,$timeout,olData,olHelpers,shared) {
+       var url = "https://dhis.moh.go.tz/api/analytics.json?dimension=dx:ZnTi99UdGCS;lMFKZN3UaYp&dimension=ou:LEVEL-2;m0frOspS7JY&dimension=pe:2014Q1;2014Q2;2014Q3;2014Q4&displayProperty=NAME";
 
     });
