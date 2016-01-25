f898e0c7b6202cd56767eeff24a35ce51524ce84
kelvinmbwilo <kelvinmbwilo@gmial.com>
2016-01-25 15:27:41 +0300

    remove dots on line charts

--- controllers/family_Planning/parcentage1Ctrl.js
+++ controllers/family_Planning/parcentage1Ctrl.js
@@ -196,7 +196,7 @@ angular.module("hmisPortal")
                         }
                     });
 
-                    var chartObject = angular.copy(portalService.chartObject);
+                    var chartObject = angular.copy(FPManager.defaultChartObject);
 
                     chartObject.title.text ="Percent of Facilities Providing Pills or Condoms through CBD," +$scope.selectedPeriod;
                     chartObject.yAxis.title.text ="% of Facilities";
