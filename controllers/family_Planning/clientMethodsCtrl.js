527a494a47c5c55736cac939767e3f1b5bb9dbf6
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2016-02-01 13:33:40 +0300

    minor corrections

--- controllers/family_Planning/clientMethodsCtrl.js
+++ controllers/family_Planning/clientMethodsCtrl.js
@@ -233,7 +233,7 @@ angular.module("hmisPortal")
 
             },
             {
-                title:'Family Planning clients by Method through Routine Facility-Based Service '+$scope.selectedPeriod,
+                title:'Family Planning clients by Method through Routine Facility-Based Service Jan 2014 to Dec 2014',
                 description:'Total Clients Monthly',
                 cardClass:"col s12 m12",
                 data:$scope.methods,
