862cc894b900be60448627780b23f7a2807b66dc
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2016-03-24 08:01:09 +0300

    fixing home page items

--- controllers/family_Planning/HomeControllers/HWTrainedCtrl.js
+++ controllers/family_Planning/HomeControllers/HWTrainedCtrl.js
@@ -826,25 +826,23 @@ angular.module("hmisPortal")
 
 
 
-        $scope.getNumberPerOu1 = function(arr,ou,arr2,pe,type,method){
+        $scope.getNumberPerOu1 = function(arr,ou,arr2,pe,method){
             var count = 0;
             angular.forEach(arr,function(value){
-                if ($scope.orgUnitType(value.organisationUnitGroups,type)) {
-                    angular.forEach(value.ancestors, function (val) {
-                        if ((ou.indexOf(';') > -1)) {
-                            var orgArr = ou.split(";");
-                            $.each(orgArr, function (c, j) {
-                                if (j == val.id) {
-                                    count++;
-                                }
-                            });
-                        } else {
-                            if (ou == val.id) {
+                angular.forEach(value.ancestors, function (val) {
+                    if ((ou.indexOf(';') > -1)) {
+                        var orgArr = ou.split(";");
+                        $.each(orgArr, function (c, j) {
+                            if (j == val.id) {
                                 count++;
                             }
+                        });
+                    } else {
+                        if (ou == val.id) {
+                            count++;
                         }
-                    });
-                }
+                    }
+                });
             });
 
             var num = $scope.getDataFromUrl1(arr2,ou,pe,method);
