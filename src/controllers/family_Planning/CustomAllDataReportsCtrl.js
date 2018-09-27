51da0a09b77f87b6cf0a236a83b579717200d911
ibrahim Wickama <ibrahimwickama@gmail.com>
2018-09-27 18:37:47 +0300

    fix(custom-reports): atuo mutations on report headers

--- src/controllers/family_Planning/CustomAllDataReportsCtrl.js
+++ src/controllers/family_Planning/CustomAllDataReportsCtrl.js
@@ -5,9 +5,17 @@ angular.module("hmisPortal")
     })
     .controller("customReportsCtrl",function ($rootScope,$scope,$http,$location,$timeout,olData,olHelpers,shared,portalService,FPManager) {
         //displaying loading during page change
+        $scope.showstaffedOptions = false;
+        $scope.showclientsOptions = false;
+        $scope.showfacilityOptions = false;
+        $scope.showserviceIntegrationOptions = false;
+        $scope.showstockOptions = false;
+        $scope.currentOptionLabel = '';
         $scope.endDate = '';
+        $scope.selectedReport = {};
         $scope.outreachData = [];
         $scope.tableHeader = [];
+        $scope.reportOptions = [];
         $scope.startDate = '';
         $scope.showReportSection = false;
         $scope.customDate = false;
@@ -30,52 +38,156 @@ angular.module("hmisPortal")
         $scope.clientsServiceAggeregateDataViewHeader = [{name: 'S/N', id:''}, {name: 'Zone', id: ''},
             {name: 'Region', id: ''}, {name: 'District', id: ''}, {name: 'Period', id: ''}, {name: 'Total FP Facilities', id: ''},{name: 'Total FP facilities reporting the FP template', id: ''},
             {name: 'Total clients of injectables facility based', id: ''},{name: 'Reporting the FP template', id: ''},{name: 'Total HWs trained in IUCD services', id: ''}];
-
-
         $scope.staffedHWstrained = [
             {name: 'Short Acting',active: '',
-                childrens: [{name: 'Short Acting',active: '', indicator: {name: 'Total Short acting', id:''},children: []}]},
+                aggregateIndicator: [{name: 'Total Short acting', id:''}],
+                facilityIndicator: [{name: 'Total Short acting', id:''}]
+               },
             {name: 'Implants',active: '',
-                childrens: [{name: 'Implants insertions',active: '', indicator: {name: 'Total Implants insertions', id:''},children: []},
-                    {name: 'Implants removal',active: '', indicator: {name: 'Total Implants removals', id:''},children: []} ]},
+                aggregateIndicator: [{name: 'Total Short acting', id:''}],
+                facilityIndicator: [{name: 'Total Short acting', id:''}]},
             {name: 'UICDs',active: '',
-                childrens: [{name: 'UICDs insertion',active: '', indicator: {name: 'Total UICDs insertions', id:''},children: []},
-                    {name: 'UICDs removal',active: '', indicator: {name: 'Total UICDs removals', id:''},children: []}]}
+                aggregateIndicator: [{name: 'Facilities staffed with HWs trained in IUCD services', id: ''},
+                    {name: 'Facilities staffed with 2 or more HWs trained in IUCD services', id: ''}, {name: 'Total HWs trained in IUCD services', id: ''}],
+                facilityIndicator: [{name: 'Total HWs trained in IUCD services', id: ''}],
+            },
+            {name:'NSV', active:'',
+                aggregateIndicator:[],
+                facilityIndicator:[]
+            },
+            {name:'Minilap', active:'',
+                aggregateIndicator:[],
+                facilityIndicator:[]
+            }
             ];
+        $scope.reportTypes = [
+            {name:'Facility data report', id:'1'},
+            {name:'Training data report', id:'2' },
+            {name:'Clients & services data report', id:'3'},
+            {name:'History of provision data report', id:'4'},
+            {name:'Commodities data report', id:'5' },
+            {name:'Service integration data report', id:'6' },
+        ];
+        $scope.aggregationTypes = [{name:'Aggregate data',id:'1'}, {name:'Facility data',id:'2'}];
+        $scope.outreachData =[
+            {name:'Short Acting',active: '',indicator: {name: 'Total Short acting outreach', id:''}},
+            {name: 'Implants insertions',active: '', indicator: {name: 'Total Implants insertions outreach', id:''}},
+            {name: 'Implants removal',active: '', indicator: {name: 'Total Implants removals outreach', id:''}},
+            {name: 'UICDs insertion',active: '', indicator: {name: 'Total UICDs insertions outreach', id:''}},
+            {name: 'UICDs removal',active: '', indicator: {name: 'Total UICDs removals outreach', id:''}}
+        ];
+        $scope.cbdServiceData =[
+            {name:'Male condoms',active: '',indicator: {name: 'Total male condoms CBD', id:''}},
+            {name: 'Female condoms',active: '', indicator: {name: 'Total female condoms CBD', id:''}},
+            {name: 'Oral pills',active: '', indicator: {name: 'Total oral pills CBD', id:''}},
+        ];
+        $scope.routineFacilityData =[
+            {name:'Male condoms',active: '',indicator: {name: 'Total clients of male condoms Facility based', id:''}},
+            {name: 'Female condoms',active: '', indicator: {name: 'Total clients of  female condoms Facility based', id:''}},
+            {name: 'Oral pills',active: '', indicator: {name: 'Total clients of  oral pills Facility based', id:''}},
+            {name: 'Injectables',active: '', indicator: {name: 'Total clients of injectables Facility based', id:''}},
+            {name: 'Implants insertions',active: '', indicator: {name: 'Total clients of Implants insertions Facility based', id:''}},
+            {name: 'Implants removal',active: '', indicator: {name: 'Total clients of Implants removals Facility based', id:''}},
+            {name: 'UICDs insertion',active: '', indicator: {name: 'Total clients of UICDs insertions Facility based', id:''}},
+            {name: 'UICDs removal',active: '', indicator: {name: 'Total clients of UICDs removals Facility based', id:''}},
+            {name: 'NSV',active: '', indicator: {name: 'Total clients of NSV Facility based', id:''}},
+            {name: 'Minilap',active: '', indicator: {name: 'Total clients of Minilap Facility based', id:''}}
+        ];
 
+        $scope.selectedReportType = function (reportType) {
+            angular.forEach($scope.reportTypes, function (report) {
+                if(report.id === '1' && reportType === '1'){
 
+                    // $scope.reportOptions = report.options;
+                } else if (report.id === '2' && reportType === '2'){
+                    console.log(report.id)
+                    $scope.showstaffedOptions = true;
+                    $scope.showfacilityOptions = false;
+                    $scope.showclientsOptions = false;
+                    $scope.showserviceIntegrationOptions = false;
+                    $scope.showstockOptions = false;
+                } else if (report.id === '3' && reportType === '3'){
+                    $scope.showstaffedOptions = false;
+                    $scope.showfacilityOptions = false;
+                    $scope.showclientsOptions = true;
+                    $scope.showserviceIntegrationOptions = false;
+                    $scope.showstockOptions = false;
+                } else if (report.id === '4' && reportType === '4'){
+                    $scope.showstaffedOptions = false;
+                    $scope.showfacilityOptions = true;
+                    $scope.showclientsOptions = false;
+                    $scope.showserviceIntegrationOptions = false;
+                    $scope.showstockOptions = false;
+                } else if (report.id === '5' && reportType === '5'){
+                    $scope.showstaffedOptions = false;
+                    $scope.showfacilityOptions = false;
+                    $scope.showclientsOptions = false;
+                    $scope.showserviceIntegrationOptions = false;
+                    $scope.showstockOptions = true;
+                } else if (report.id === '6' && reportType === '6'){
+                    $scope.showstaffedOptions = false;
+                    $scope.showfacilityOptions = false;
+                    $scope.showclientsOptions = false;
+                    $scope.showserviceIntegrationOptions = true;
+                    $scope.showstockOptions = false;
+                }
+            });
+        };
         $scope.updateOutreachCBD = function (staffed) {
-            var currentNewOutreach = []
             angular.forEach( $scope.staffedHWstrained, function (item) {
                 if (item.name === staffed.name) {
-                    if (item.active === 'active') {
+                    if (item.active === 'actived') {
                         // hence remove its contents to other list groups
                         item.active = '';
-                        $scope.outreachData = $scope.itemsOnArrayRemoval($scope.outreachData, staffed['childrens'], 'name');
+                        $scope.aggregateDataViewHeader = $scope.itemsOnTableHeaderRemoval($scope.aggregateDataViewHeader, staffed.indicator, 'name');
                     } else {
                         // hence add its contents to other list groups
-                        item.active = 'active';
-                        $scope.outreachData = $scope.outreachData.concat(staffed['childrens'])
+                        item.active = 'actived';
+                        $scope.aggregateDataViewHeader = $scope.aggregateDataViewHeader.concat(staffed.aggregateIndicator)
                     }
 
                 }
             });
         };
-
-        $scope.updatingsFromOutreachCBD = function (data){
+        $scope.updatingsOutreachOptionsToTable = function (data){
             angular.forEach( $scope.outreachData, function (item) {
                 if (item.name === data.name) {
-                    if (item.active === 'active') {
+                    if (item.active === 'actived') {
                         item.active = '';
                         $scope.aggregateDataViewHeader = $scope.itemsOnTableHeaderRemoval($scope.aggregateDataViewHeader, data.indicator, 'name')
                     } else {
-                        item.active = 'active';
+                        item.active = 'actived';
                         $scope.aggregateDataViewHeader.push(data.indicator)
                     }
                 }
-            })
+            });
+        };
+        $scope.updatingsCBDtoTable = function (data){
+            angular.forEach( $scope.cbdServiceData, function (item) {
+                if (item.name === data.name) {
+                    if (item.active === 'actived') {
+                        item.active = '';
+                        $scope.aggregateDataViewHeader = $scope.itemsOnTableHeaderRemoval($scope.aggregateDataViewHeader, data.indicator, 'name')
+                    } else {
+                        item.active = 'actived';
+                        $scope.aggregateDataViewHeader.push(data.indicator)
+                    }
+                }
+            });
         }
-
+        $scope.updatingsRoutineDatatoTable = function (data){
+            angular.forEach( $scope.routineFacilityData, function (item) {
+                if (item.name === data.name) {
+                    if (item.active === 'actived') {
+                        item.active = '';
+                        $scope.aggregateDataViewHeader = $scope.itemsOnTableHeaderRemoval($scope.aggregateDataViewHeader, data.indicator, 'name')
+                    } else {
+                        item.active = 'actived';
+                        $scope.aggregateDataViewHeader.push(data.indicator)
+                    }
+                }
+            });
+        }
         $scope.removeDuplicates= function(originalArray, key) {
             var newArray = [];
             var lookupObject  = {};
@@ -88,7 +200,6 @@ angular.module("hmisPortal")
             }
             return newArray;
         }
-
         $scope.itemsOnArrayRemoval = function(collection, removals, key){
             angular.forEach(collection, function (collectedItem, index) {
                 angular.forEach(removals, function (removalItem) {
@@ -99,7 +210,6 @@ angular.module("hmisPortal")
             });
             return collection;
         }
-
         $scope.itemsOnTableHeaderRemoval = function(collection, removal, key){
             angular.forEach(collection, function (collectedItem, index) {
                     if(collectedItem[key] === removal[key]){
@@ -108,7 +218,6 @@ angular.module("hmisPortal")
             });
             return collection;
         }
-
         $scope.previewReport = function() {
             $scope.showReportSection = !$scope.showReportSection;
             console.log($scope.startDate, $scope.endDate);
