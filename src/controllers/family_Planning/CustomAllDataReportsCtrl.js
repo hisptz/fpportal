eba7da233f8576ad4e226271baad4eed2934a036
ibrahim Wickama <ibrahimwickama@gmail.com>
2018-09-28 18:45:16 +0300

    feat(custom-reports): import period component

--- src/controllers/family_Planning/CustomAllDataReportsCtrl.js
+++ src/controllers/family_Planning/CustomAllDataReportsCtrl.js
@@ -25,6 +25,7 @@ angular.module("hmisPortal")
         $scope.startDate = '';
         $scope.showReportSection = false;
         $scope.customDate = false;
+        $scope.selectedPeriodType = '';
         $scope.periodType = 'monthly';
         $scope.reportHeader = 'Aggregate data view';
         $scope.aggregateDataViewHeader = [{name: 'S/N', id:''}, {name: 'Zone', id: ''},
@@ -99,6 +100,22 @@ angular.module("hmisPortal")
             {name: 'NSV',active: '', indicator: {name: 'Total clients of NSV Facility based', id:''}},
             {name: 'Minilap',active: '', indicator: {name: 'Total clients of Minilap Facility based', id:''}}
         ];
+
+        $scope.periodTypes = [
+            {value: 'Monthly', name: 'Monthly', shown: true},
+            {value: 'Quarterly', name: 'Quarterly', shown: false},
+            {value: 'Yearly', name: 'Yearly', shown: true},
+            {value: 'RelativeMonth', name: 'Relative Month', shown: false},
+            {value: 'RelativeQuarter', name: 'Relative Quarter', shown: false},
+            {value: 'RelativeYear', name: 'Relative Year', shown: false},
+        ];
+        $scope.availablePeriods = [];
+        $scope.selectedPeriods = [];
+
+
+
+
+
 
         $scope.selectedReportType = function (reportType) {
             angular.forEach($scope.reportTypes, function (report) {
@@ -275,4 +292,128 @@ angular.module("hmisPortal")
             return Math.floor((Math.random() * 500) + 100)
         }
 
+            // PERIOD COMPONENT FUNCTIONS
+        $scope.pushPeriodForward = function (e){
+            $scope.selectedYear++;
+            $scope.updatePeriodType($scope.periodType, '')
+        }
+        $scope.pushPeriodBackward = function (e){
+            $scope.selectedYear--;
+            $scope.updatePeriodType($scope.periodType, '')
+        }
+        $scope.updatePeriodType = function (periodType, e) {
+           $scope.periodType = periodType
+            $scope.availablePeriods = $scope.getPeriodsBasedOnType(periodType, $scope.selectedYear);
+        }
+        $scope.getPeriodsBasedOnType = function(periodType, year) {
+            switch (periodType) {
+                case 'Monthly':
+                    return this._getMonthlyPeriods(year);
+                case 'Quarterly':
+                    return this._getQuarterlyPeriods(year);
+                case 'Yearly':
+                    return this._getYearlyPeriods(year);
+                case 'RelativeMonth':
+                    return this._getRelativeMonthPeriods();
+                case 'RelativeQuarter':
+                    return this._getRelativeQuarterPeriods();
+                case 'RelativeYear':
+                    return this._getRelativeYearPeriods();
+                default:
+                    return [];
+            }
+        }
+        $scope._getMonthlyPeriods = function(year) {
+            const periods = [{id: year + '12', name: 'December ' + year}, {id: year + '11', name: 'November ' + year}, {
+                id: year + '10',
+                name: 'October ' + year
+            }, {id: year + '09', name: 'September ' + year}, {id: year + '08', name: 'August ' + year}, {
+                id: year + '07',
+                name: 'July ' + year
+            }, {id: year + '06', name: 'June ' + year}, {id: year + '05', name: 'May ' + year}, {
+                id: year + '04',
+                name: 'April ' + year
+            }, {id: year + '03', name: 'March ' + year}, {id: year + '02', name: 'February ' + year}, {
+                id: year + '01',
+                name: 'January ' + year
+            }];
+
+            const currentDate = new Date();
+            const currentMonth = currentDate.getMonth() + 1;
+            const currentPeriod = currentDate.getFullYear() + (currentMonth < 10 ? ('0' + currentMonth) : currentMonth).toString();
+            return periods.map((period) => {
+                return {
+                    ...period,
+                    type: 'Monthly',
+                };
+            }).filter((period) => parseInt(period.id, 10) <= parseInt(currentPeriod, 10));
+        }
+        $scope._getQuarterlyPeriods = function (year) {
+            const periods = [{id: year + 'Q4', name: 'October - December ' + year}, {
+                id: year + 'Q3',
+                name: 'July - September ' + year
+            }, {id: year + 'Q2', name: 'April - June ' + year}, {
+                id: year + 'Q1',
+                name: 'January - March ' + year
+            }];
+
+            return periods.map((period) => {
+                period.type = 'Quarterly';
+                return period;
+            });
+        }
+        $scope._getYearlyPeriods = function (year) {
+            const periods = [];
+            for (let i = 0; i <= 10; i++) {
+                const useYear = parseInt(year, 10) - i;
+                periods.push({id: useYear.toString(), name: useYear.toString(), type: 'Yearly'});
+            }
+
+            return periods;
+        }
+        $scope._getRelativeMonthPeriods = function () {
+            const periods = [{id: 'THIS_MONTH', name: 'This Month'}, {id: 'LAST_MONTH', name: 'Last Month'}, {
+                id: 'LAST_3_MONTHS',
+                name: 'Last 3 Months'
+            }, {id: 'LAST_6_MONTHS', name: 'Last 6 Months'}, {id: 'LAST_12_MONTHS', name: 'Last 12 Months'}];
+
+            return periods.map((period) => {
+                period.type = 'RelativeMonth';
+                return period;
+            });
+        }
+        $scope._getRelativeQuarterPeriods = function () {
+            const periods = [{id: 'THIS_QUARTER', name: 'This Quarter'}, {
+                id: 'LAST_QUARTER',
+                name: 'Last Quarter'
+            }, {id: 'LAST_4_QUARTERS', name: 'Last 4 Quarters'}];
+
+            return periods.map((period) => {
+                period.type = 'RelativeQuarter';
+                return period;
+            });
+        }
+        $scope._getRelativeYearPeriods = function() {
+            const periods = [{id: 'THIS_YEAR', name: 'This Year'}, {
+                id: 'LAST_YEAR',
+                name: 'Last Year'
+            }, {id: 'LAST_5_YEARS', name: 'Last 5 Years'}];
+
+            return periods.map((period) => {
+                period.type = 'RelativeYear';
+                return period;
+            });
+
+        }
+        $scope.toggleAvailablePeriod = function (selectedPeriod, e) {
+            $scope.selectedPeriods.push(selectedPeriod);
+        };
+        $scope.toggleSelectedPeriod = function (selectedPeriod, e) {
+            angular.forEach($scope.selectedPeriods, function (period, index) {
+                if(period.id === selectedPeriod.id ){
+                    $scope.selectedPeriods.splice(index,1);
+                }
+            })
+        }
+            // END OF PERIOD COMPONENT FUNCTIONS
     });
